20853c21bebe153c591370c19062855cef27986e
ezl <ericzliu@gmail.com>
2015-03-04 10:48:28 -0500

    touch

--- js/annotations.js
+++ js/annotations.js
@@ -1,11 +1,23 @@
 $(document).ready(function () {
-  $('[data-content]').mouseenter(function () {
+  $('[data-content]').mouseenter(function() {
       $('[data-annotation]').hide();
       key = $(this).attr("data-content");
       target = $('[data-annotation=' + key + ']');
       target.show();
   });
-  $('[data-content]').mouseleave(function () {
+  $('[data-content]').mouseleave(function() {
       $('[data-annotation]').fadeOut();
+  });
+
+  $('.hide-offcanvas').bind("touchend", function() {
+      $('[data-annotation]').fadeOut();
+      $('.row-offcanvas').removeClass('active');
+  });
+  $('[data-content]').bind("touchend", function() {
+      $('[data-annotation]').hide();
+      key = $(this).attr("data-content");
+      target = $('[data-annotation=' + key + ']');
+      target.show();
+      $('.row-offcanvas').addClass('active');
   });
 });
